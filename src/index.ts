e2d48e153cbd37b1157317a3d5d39f2917477786
Rachel Nehmer <rachel@nuanced.dev>
2025-04-16 13:06:15 -0700

    Expose functions and entry points
    Renamed executable to avoid namespace issues and to allow comparing with jelly proper
    Add prepare script so we can install as node module

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+// Core analysis functionality
+export { analyzeFiles } from "./analysis/analyzer";
+export { default as Solver } from "./analysis/solver";
+export { FragmentState } from "./analysis/fragmentstate";
+export { GlobalState } from "./analysis/globalstate";
+
+// Options management
+export {
+    options,
+    setOptions,
+    resolveBaseDir,
+    setDefaultTrackedModules,
+    setPatternProperties
+} from "./options";
+
+// Essential types
+export { ConstraintVar } from "./analysis/constraintvars";
+export { Token, FunctionToken, ObjectToken } from "./analysis/tokens";
+export { AccessPath } from "./analysis/accesspaths";
+export { FunctionInfo, ModuleInfo } from "./analysis/infos";
+export {
+    FilePath,
+    Location,
+    SimpleLocation,
+    locationToString,
+    locationToStringWithFile,
+    locationToStringWithFileAndEnd,
+    locationContains,
+    locationIn,
+    longestCommonPrefix
+} from "./misc/util";
+
+// Result processing
+export { AnalysisStateReporter } from "./output/analysisstatereporter";
+export { compareCallGraphs } from "./output/compare";
+export { TokenListener } from "./analysis/listeners";
+export { exportCallGraphHtml, exportDataFlowGraphHtml } from "./output/visualizer";
+
+// File utilities
+export {
+    expand,
+    isShebang,
+    isLocalRequire,
+    requireResolve,
+    autoDetectBaseDir,
+    codeFromLocation,
+    writeStreamedStringify
+} from "./misc/files";
